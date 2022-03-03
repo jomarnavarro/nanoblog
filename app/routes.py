63871ef2fda70ca39dce8458d5b8fbbc3f0eae3f
omar navarro <jomarnavarro@gmail.com>
2022-03-02 18:24:25 -0600

    Adds templating to nanoblog

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
+from flask import render_template
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-	return "<h1>Hello world!</h1>"
+	user = {'username': 'Omar'}
+	posts = [
+		{
+			'author': {'username': 'omar'},
+			'body': 'Today was a sunny day in Mexico City'
+		},
+		{
+			'author': {'username': 'chank'},
+			'body': 'A poor politician is a lousy politician'
+		},
+		{
+			'author': {'username': 'csalinas'},
+			'body': 'I hear no evil and see no evil.'
+		}
+	]
+	return render_template('index.html', title='Index', user=user, posts=posts)
 
